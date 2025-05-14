1052ac576f1c92cbfbb082bb92b4aa8fa5ddda8a
IT24103989 <it24103989@my.sliit.lk>
2025-05-15 00:10:25 +0530

    create servlet to delete vendor

---
+++ WeddingPlanning/src/main/java/com/WeddingPlanner/servlets/DeleteVendorServlet.java
@@ -0,0 +1,38 @@
+//IT24103989
+package com.WeddingPlanner.servlets;
+
+import com.WeddingPlanner.services.UserAuthService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/deleteVendor")
+public class DeleteVendorServlet extends HttpServlet {
+    private UserAuthService userService;
+
+    @Override
+    public void init() throws ServletException {
+        userService = new UserAuthService(getServletContext());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        if (username != null && !username.trim().isEmpty()) {
+            boolean deleted = userService.deleteVendorByUsername(username);
+            if (deleted) {
+                out.write("{\"status\":\"success\"}");
+            } else {
+                out.write("{\"status\":\"failed\", \"message\":\"Vendor not found or couldn't delete\"}");
+            }
+        } else {
+            out.write("{\"status\":\"failed\", \"message\":\"Invalid username\"}");
+        }
+    }
+}
